57aa5a8e5599f633464c6f467bd90b357f90e415
Niklas Niemelä <niklas.niemela@hotmail.com>
2018-12-21 22:53:34 +0200

    Updates

---
+++ precache-manifest.8f781449df558703e31c2304b6ab1095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d94b43e52872063077",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Niosni/static/js/runtime~main.42d94b43.js"
+  },
+  {
+    "revision": "b0c99b41fcb4e22ecc69",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Niosni/static/js/main.b0c99b41.chunk.js"
+  },
+  {
+    "revision": "d235feaebd55b686612c",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Niosni/static/js/1.d235feae.chunk.js"
+  },
+  {
+    "revision": "b0c99b41fcb4e22ecc69",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Niosni/static/css/main.d666529d.chunk.css"
+  },
+  {
+    "revision": "d235feaebd55b686612c",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Niosni/static/css/1.20525d08.chunk.css"
+  },
+  {
+    "revision": "6f44552e3cf9c8b686a1c191f550b305",
+    "url": "/it-teaching-abo-akademi/2018-interactive-web-apps-project-Niosni/index.html"
+  }
+];
